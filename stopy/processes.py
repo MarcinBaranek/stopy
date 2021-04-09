5bbc150943618e671d117f587f7d527486f382bf
baranek <baranekmarcin47@gmail.com>
2021-04-09 17:16:11 +0200

    update visualy view

--- stopy/processes.py
+++ stopy/processes.py
@@ -2,23 +2,22 @@ import numpy as np
 
 
 class Wiener:
-    """ Wiener process generator
+    """Wiener process generator
     ============================================
     EXAMPLES:
-        W = Wiener()
-
         # generate process to plot
-        to_plot = W.generate_array(np.zeros(shape=(3,)), x_axis=True)
+        to_plot = Wiener.generate_array(np.zeros(shape=(3,)), x_axis=True)
         plt.plot(to_plot[0], to_plot[1])
         plt.show()
 
         # generate process on grid
         grid = np.array([0.0, 1.9, 10.0, 30.0, 30.1, 32])
-        plt.plot(grid, W.generate_on_grid(grid, start_point=np.zeros(shape=(4,))))
+        plt.plot(grid,
+            Wiener.generate_on_grid(grid, start_point=np.zeros(shape=(4,))))
         plt.show()
 
         # do something until the process is in unit 5 dimensional ball
-        trace = W.generate(np.zeros(shape=(5,)))
+        trace = Wiener.generate(np.zeros(shape=(5,)))
         point = trace.__next__()
         do_something(point)
         while (point ** 2).sum() < 1:
@@ -27,59 +26,68 @@ class Wiener:
     ============================================
     """
     def __init__(self, *args, **kwargs):
-        """ empty init, because it is fully static class
+        """Empty init, because it is fully static class
         """
         pass
 
     @staticmethod
     def generate(start_point=None, time=1, division=500):
-        """ function generating the process
-        :param start_point: numpy.array with shape (n,), starting point of the process
-        :param time: time interval, if time < 0, then function will be still generating process
+        """Function generating the process
+        :param start_point: numpy.array with shape (n,),
+            initial point of the process
+        :param time: time interval, if time < 0,
+            then function will be still generating process
         :param division: number of intermediate points in a unit time segment
         :return: generator generating successive realizations of the process
         """
-        yield start_point if start_point is not None else np.zeros(shape=(1,))
+        _point = np.zeros(shape=(1,)) if start_point is None else start_point
+        size = _point.shape
         if time < 0:
             while True:
-                start_point = start_point + np.random.normal(loc=0.0, scale=1 / np.sqrt(division),
-                                                             size=start_point.shape)
-                yield start_point
+                yield _point
+                _point = _point + np.random.normal(size) / np.sqrt(division)
         else:
-            for _ in range(int(time * division) - 1):
-                start_point = start_point + np.random.normal(loc=0.0, scale=1 / np.sqrt(division),
-                                                             size=start_point.shape)
-                yield start_point
+            for _ in range(int(time * division)):
+                yield _point
+                _point = _point + np.random.normal(size) / np.sqrt(division)
 
     @staticmethod
     def generate_on_grid(_grid, start_point=None):
-        """ function generating the process on grid
+        """Function generating the process on grid
         :param _grid: sorted list or array with grid
-        :param start_point: numpy.array with shape (n,), starting point of the process
+        :param start_point: numpy.array with shape (n,),
+            starting point of the process
         :return: array with realizations of the Wiener process on grid
          """
-        _array = [start_point] if start_point is not None else [np.zeros(shape=(1,))]
+        _arr = [np.zeros(shape=(1,))] if start_point is None else [start_point]
+        size = _arr[0].shape
         for i in range(1, len(_grid), 1):
-            _array.append(_array[-1] + np.random.normal(loc=0.0, scale=1 / np.sqrt(_grid[i] - _grid[i - 1]),
-                                                        size=start_point.shape))
-        return np.array(_array)
+            _arr.append(_arr[-1] + np.random.normal(size)
+                        / np.sqrt(_grid[i] - _grid[i - 1]))
+        return np.array(_arr)
 
     @staticmethod
     def generate_array(start_point=None, time=1, x_axis=False, division=500):
-        """ function generating the process and return array of
-        :param start_point: numpy.array with shape (n,), starting point of the process
+        """Function generating the process and return array of
+        :param start_point: numpy.array with shape (n,),
+            starting point of the process
         :param time: time interval, should be greater than 0
-        :param x_axis: bool, if True return array with process and grid, else return only array with process
+        :param x_axis: bool, if True return array with process and grid,
+            else return only array with process
         :param division: number of intermediate points in a unit time segment
-        :return: grid with array or only array with realizations of the Wiener process
+        :return: grid with array or only array with realizations of the process
         """
-        start_point = start_point if start_point is not None else np.zeros(shape=(1,))
-        _array = np.array([element for element in Wiener.generate(start_point, time=time, division=division)])
-        return (np.linspace(0.0, time, int(time * division) + 1), _array) if x_axis else _array
+        _point = np.zeros(shape=(1,)) if start_point is None else start_point
+        _arr = np.array(
+            [element for element in Wiener.generate(_point,
+                                                    time=time,
+                                                    division=division)])
+        return (np.linspace(0.0, time, int(time * division) + 1), _arr)\
+            if x_axis else _arr
 
 
 class Poisson:
-    """ Poisson process generator
+    """Poisson process generator
     ============================================
     EXAMPLES:
         to_plot = Poisson.generate_jumps(dim=4, time=10)
@@ -88,34 +96,37 @@ class Poisson:
         plt.show()
 
         grid = np.array([0.0, 1.9, 10.0, 30.0, 30.1, 32])
-        for arr in Poisson.generate_on_grid(grid, start_point=np.zeros(shape=(5,))):
+        for arr in Poisson.generate_on_grid(grid,
+                                            start_point=np.zeros(shape=(5,))):
             plt.step(arr, np.arange(len(arr)))
         plt.show()
     ============================================
     """
     def __init__(self, *args, **kwargs):
-        """ empty init, because it is fully static class
+        """Empty init, because it is fully static class
         """
         pass
 
     @staticmethod
     def _generate_incrementally(lam, start_point=None, time=1, division=500):
-        """ internal function generating the process
+        """Internal function generating the process
         :param lam: float, the intensity of the process
-        :param start_point: numpy.array with shape (n,), starting point of the process
-        :param time: time interval, if time < 0, then function will be still generating process
+        :param start_point: numpy.array with shape (n,),
+            starting point of the process
+        :param time: time interval, if time < 0,
+            then function will be still generating process
         :param division: number of intermediate points in a unit time segment
         :return: generator generating successive realizations of the process
         """
-        start_point = start_point if start_point is not None else np.zeros(shape=(1,))
-        yield start_point
-        for _ in range(int(time * division) - 1):
-            start_point = start_point + np.random.poisson(lam / division, size=start_point.shape)
-            yield start_point
+        _point = np.zeros(shape=(1,)) if start_point is None else start_point
+        size = _point.shape
+        for _ in range(int(time * division)):
+            yield _point
+            _point = _point + np.random.poisson(lam / division, size=size)
 
     @staticmethod
     def generate_one_dim_jumps(lam=1, time=1):
-        """ generates the one dimensional process on the interval [0, time]
+        """Generates the one dimensional process on the interval [0, time]
         :param lam: float, the intensity of the process
         :param time: time interval
         :return: numpy.array with jumps of the process
@@ -127,7 +138,7 @@ class Poisson:
 
     @staticmethod
     def generate_jumps(lam=1, dim=1, time=1):
-        """ generates the process on the interval [0, time]
+        """Generates the process on the interval [0, time]
         :param lam: float, the intensity of the process
         :param dim: dimensionality of the process
         :param time: time interval
@@ -140,23 +151,26 @@ class Poisson:
 
     @staticmethod
     def generate_on_grid(_grid, lam=1, start_point=None):
-        """ function generating the process on grid
+        """Function generating the process on grid
         :param _grid: sorted list or array with grid
         :param lam: float, the intensity of the process
-        :param start_point: numpy.array with shape (n,), starting point of the process
+        :param start_point: numpy.array with shape (n,),
+         starting point of the process
         :return: array with realizations of the Poisson process on grid
          """
-        _array = [start_point] if start_point is not None else np.zeros(shape=(1,))
+        _arr = [np.zeros(shape=(1,))] if start_point is None else start_point
         for i in range(1, len(_grid), 1):
-            _array.append(_array[-1] + np.random.poisson(lam=lam * (_grid[i] - _grid[i - 1]), size=start_point.shape))
-        return np.array(_array).transpose()
+            _arr.append(_arr[-1] + np.random.poisson(
+                lam=lam * (_grid[i] - _grid[i - 1]), size=start_point.shape))
+        return np.array(_arr).transpose()
 
 
 class OrnsteinUhlenbeck:
-    """ Ornstein-Uhlenbeck process generator
-    The Ornstein–Uhlenbeck process X_t is defined by the following stochastic differential equation:
+    """Ornstein-Uhlenbeck process generator
+    The Ornstein–Uhlenbeck process X_t is defined by the following
+    stochastic differential equation:
         dX_t = theta (mu - X_t) dt + sigma dW_t
-    where theta, sigma >0 are parameters and W_t denotes the Wiener process.
+    where theta, sigma > 0 are parameters and W_t denotes the Wiener process.
     Solution:   # use f(t, x) = x exp(theta t)
 
     X_t = X_0 exp(-theta t) + mu (1 - exp(-theta t)) +
@@ -165,13 +179,15 @@ class OrnsteinUhlenbeck:
     ============================================
     EXAMPLES:
         # generate process to plot
-        to_plot = OrnsteinUhlenbeck.generate_array(np.zeros(shape=(3,)), x_axis=True)
+        to_plot = OrnsteinUhlenbeck.generate_array(
+            np.zeros(shape=(3,)), x_axis=True)
         plt.plot(to_plot[0], to_plot[1])
         plt.show()
 
         # generate process on grid
         grid = np.array([0.0, 1.9, 10.0, 30.0, 30.1, 32])
-        plt.plot(grid, OrnsteinUhlenbeck.generate_on_grid(grid, start_point=np.zeros(shape=(4,))))
+        plt.plot(grid, OrnsteinUhlenbeck.generate_on_grid(
+            grid, start_point=np.zeros(shape=(4,))))
         plt.show()
 
         # do something until the process is in unit 5 dimensional ball
@@ -184,17 +200,19 @@ class OrnsteinUhlenbeck:
     ============================================
     """
     def __init__(self, *args, **kwargs):
-        """ empty init, because it is fully static class
+        """Empty init, because it is fully static class
         """
         pass
 
     @staticmethod
     def _eval_first_part(theta=1, mu=0, start_point=None, time=1):
-        """ compute deterministic part of process:
+        """Compute deterministic part of process:
         X_0 exp(-theta t) + mu (1 - exp(-theta t))
         :param theta: float > 0, parameter of process
-        :param mu: numpy.array with the same shape as start_point, it is a mean value of process
-        :param start_point: numpy.array with the same shape as mu, it is a initial value
+        :param mu: numpy.array with the same shape as start_point,
+            it is a mean value of process
+        :param start_point: numpy.array with the same shape as mu,
+            it is a initial value
         :param time: float > 0, time
         :return: numpy.array with the same shape as mu or start_point
         """
@@ -203,13 +221,14 @@ class OrnsteinUhlenbeck:
 
     @staticmethod
     def _eval(theta, mu, sigma, start_point, time, wiener):
-        return OrnsteinUhlenbeck._eval_first_part(theta=theta, mu=mu, start_point=start_point, time=time)\
+        return OrnsteinUhlenbeck._eval_first_part(
+            theta=theta, mu=mu, start_point=start_point, time=time)\
                + sigma * np.exp(-theta * time) * wiener / np.sqrt(2 * theta)
 
     @staticmethod
     def _eval_step(theta, mu, sigma, start_point, time, _wiener, step):
-        _wiener = _wiener + np.random.normal(loc=0.0, scale=np.sqrt(np.exp(2 * theta * time) * (np.exp(step) - 1)),
-                                             size=start_point.shape)
+        _wiener = _wiener + np.random.normal(size=start_point.shape)\
+                  * np.sqrt(np.exp(2 * theta * time) * (np.exp(step) - 1)),
         start_point = OrnsteinUhlenbeck._eval(theta=theta, mu=mu, sigma=sigma,
                                               start_point=start_point, time=time, wiener=_wiener)
         time = time + step
@@ -217,7 +236,7 @@ class OrnsteinUhlenbeck:
 
     @staticmethod
     def generate(theta=1, mu=0, sigma=1, start_point=None, time=1, division=500):
-        """ function generating the process
+        """Function generating the process
         :param sigma: float > 0, volatility parameter
         :param mu: float, mean
         :param theta: float > 0, parameter of process
@@ -243,7 +262,7 @@ class OrnsteinUhlenbeck:
 
     @staticmethod
     def generate_on_grid(_grid, theta=1, mu=0, sigma=1, start_point=None):
-        """ function generating the process on grid
+        """Function generating the process on grid
         :param _grid: sorted list or array with grid
         :param theta: float > 0, parameter of process
         :param mu: float, mean
@@ -261,7 +280,7 @@ class OrnsteinUhlenbeck:
 
     @staticmethod
     def generate_array(theta=1, mu=0, sigma=1, start_point=None, time=1, x_axis=False, division=500):
-        """ function generating the process and return array of
+        """Function generating the process and return array of
         :param sigma: float > 0, volatility parameter
         :param mu: float, mean
         :param theta: float > 0, parameter of process
--- tests/test_processes.py
+++ tests/test_processes.py
@@ -5,10 +5,14 @@ from stopy.processes import Wiener, Poisson, OrnsteinUhlenbeck
 
 class TestWiener(TestCase):
     def test_generate(self):
-        shape_div_100 = Wiener.generate_array(np.zeros(shape=(3,)), time=1.0, division=100, x_axis=False)
-        shape_div_200 = Wiener.generate_array(np.zeros(shape=(4,)), time=1.0, division=200, x_axis=False)
-        shape_time_20 = Wiener.generate_array(np.zeros(shape=(5,)), time=20.0, x_axis=False)
-        shape_time_8coma5 = Wiener.generate_array(np.zeros(shape=(6,)), time=8.5, x_axis=False)
+        shape_div_100 = Wiener.generate_array(np.zeros(shape=(3,)), time=1.0,
+                                              division=100, x_axis=False)
+        shape_div_200 = Wiener.generate_array(np.zeros(shape=(4,)), time=1.0,
+                                              division=200, x_axis=False)
+        shape_time_20 = Wiener.generate_array(np.zeros(shape=(5,)), time=20.0,
+                                              x_axis=False)
+        shape_time_8coma5 = Wiener.generate_array(np.zeros(shape=(6,)),
+                                                  time=8.5, x_axis=False)
         self.assertEqual(shape_div_100.shape, (100, 3))
         self.assertEqual(shape_div_200.shape, (200, 4))
         self.assertEqual(shape_time_20.shape, (10000, 5))
@@ -23,11 +27,16 @@ class TestWiener(TestCase):
 
 class TestPoisson(TestCase):
     def test__generate_incrementally(self):
-        arguments = {"lam": 1, "start_point": np.zeros(shape=(3,)), "time": 1, "division": 100}
-        shape_div_100 = np.array([element for element in Poisson._generate_incrementally(**arguments)])
-        shape_div_200 = Wiener.generate_array(np.zeros(shape=(4,)), time=1.0, division=200, x_axis=False)
-        shape_time_20 = Wiener.generate_array(np.zeros(shape=(5,)), time=20.0, x_axis=False)
-        shape_time_8coma5 = Wiener.generate_array(np.zeros(shape=(6,)), time=8.5, x_axis=False)
+        args = {"lam": 1, "start_point": np.zeros(shape=(3,)),
+                "time": 1, "division": 100}
+        shape_div_100 = np.array(
+            [element for element in Poisson._generate_incrementally(**args)])
+        shape_div_200 = Wiener.generate_array(np.zeros(shape=(4,)), time=1.0,
+                                              division=200, x_axis=False)
+        shape_time_20 = Wiener.generate_array(np.zeros(shape=(5,)), time=20.0,
+                                              x_axis=False)
+        shape_time_8coma5 = Wiener.generate_array(np.zeros(shape=(6,)),
+                                                  time=8.5, x_axis=False)
         self.assertEqual(shape_div_100.shape, (100, 3))
         self.assertEqual(shape_div_200.shape, (200, 4))
         self.assertEqual(shape_time_20.shape, (10000, 5))
@@ -45,7 +54,8 @@ class TestPoisson(TestCase):
 
 class TestOrnsteinUhlenbeck(TestCase):
     def test_generate(self):
-        arguments = {"theta": 1.0, "mu": 0, "sigma": 1, "start_point": np.zeros(shape=(3,)),
+        arguments = {"theta": 1.0, "mu": 0, "sigma": 1,
+                     "start_point": np.zeros(shape=(3,)),
                      "time": 1, "x_axis": False, "division": 100}
         shape_div_100 = OrnsteinUhlenbeck.generate_array(**arguments)
         arguments["division"] = 200
