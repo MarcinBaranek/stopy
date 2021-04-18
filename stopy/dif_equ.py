444f338acee3ede57c116e4d46f6f551eb8db1ab
baranek <baranekmarcin47@gmail.com>
2021-04-18 14:44:36 +0200

    writing the doc, part 1

--- stopy/dif_equ.py
+++ stopy/dif_equ.py
@@ -3,12 +3,26 @@ from scipy.stats import chisquare
 
 
 class ItoProcess:
+    """
+    # ToDO write the documentation
+    """
     def __init__(self, a_func=None, b_func=None, init_val=None):
+        """
+        # ToDO write the documentation
+        :param a_func:
+        :param b_func:
+        :param init_val:
+        """
         self.a_func = a_func if a_func is not None else lambda t, x: 0
         self.b_func = b_func if b_func is not None else lambda t, x: 1
         self.init_val = 0.0 if init_val is None else init_val
 
     def __eq__(self, other):
+        """
+        # ToDO write the documentation
+        :param other:
+        :return:
+        """
         a_check = self.a_func.__code__.co_code == other.a_func.__code__.co_code
         b_check = self.b_func.__code__.co_code == other.b_func.__code__.co_code
         init_check = self.init_val == other.init_val
@@ -19,6 +33,11 @@ class ItoProcess:
                f"{self.__repr__()}"
 
     def __add__(self, other):
+        """
+        # ToDO write the documentation
+        :param other:
+        :return:
+        """
         return ItoProcess(lambda t, x: self.a_func(t, x) + other.a_func(t, x),
                           lambda t, x: self.b_func(t, x) + other.b_func(t, x),
                           self.init_val + other.init_val)
@@ -67,6 +86,9 @@ class ItoProcess:
 
 
 class EulerScheme(ItoProcess):
+    """Euler scheme class for solving stochastic differential equations
+    # ToDO write the documentation
+    """
     def __init__(self, a_func=None, b_func=None, init_val=None):
         """Constructor for stochastic EulerScheme.
          It is used to solve character equations:
@@ -113,59 +135,3 @@ class EulerScheme(ItoProcess):
             _point = self.step(init=_point, t_0=_time,
                                time=_time + dt, steps=steps)
             _time = _time + dt
-
-
-class ItoProcess(ItoProcess):
-    def __init__(self, a_func=None, b_func=None, init_val=None):
-        """Constructor for Ito process.
-         The process is form:
-         dX_t = a(t, X_t)dt + b(t, X_t)dW_t,
-         with an initial condition X_0 = init_val
-        =========================================================
-        :param a_func: a function of two arguments, time and a spatial variable
-        :param b_func: a function of two arguments, time and a spatial variable
-        :param init_val: initial condition of the process
-        """
-        super().__init__(a_func, b_func, init_val)
-
-    def fit_test(self, data, t_arr=None, df=None, steps=10):
-        """The method checks how well the process describes the data.
-        =========================================================
-        Statistical test:
-        H_0: the data comes from the distribution described by the process
-        H_1: The data is not from the distribution described by the process
-
-        if p value < significance level then we reject the null hypothesis
-
-        References:
-        [1] Bak, J. (1998), Nonparametric methods in finance, Master’s thesis,
-         Department of Mathematical Modelling, Technical University of Denmark,
-         Lyngby. IMM-EKS-1998-34.
-        =========================================================
-        :param data: data array
-        :param t_arr: time array, default is [0, 1, 2, ..., len_data]
-        :param df: int, degrees of freedom, otherwise the number of simulations
-            default is equal to int((len_data - 6) / 5)
-        :param steps: int, the number of steps on which the Euler scheme is
-            based in each simulation
-        :return: float, p value
-        """
-        # data preparation
-        schema, len_data = EulerScheme(self.a_func, self.b_func), len(data)
-        df = int((len_data - 6) / 5) if df is None else df
-        expected = (len_data - 1) / (df + 1)
-        t_arr = np.arange(1, len_data + 1) if t_arr is None else t_arr
-        r_arr = np.ones(shape=(len_data - 1))
-
-        # computing simulations
-        for i in range(len_data - 1):
-            for _ in range(df):
-                r_arr[i] += int(
-                    schema.step(data[i], t_arr[i], t_arr[i + 1], steps)
-                    <= data[i + 1])
-
-        # preparation for the test
-        omega_arr = np.array([
-            np.array(list(map(lambda x: 1 if x == i else 0, r_arr))).sum()
-            for i in range(1, df + 1)])
-        return chisquare(omega_arr, expected)[1]
