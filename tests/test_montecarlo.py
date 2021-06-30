448556753c3403894157bcd630cc525e8ac3e261
baranek <baranekmarcin47@gmail.com>
2021-06-30 20:27:39 +0200

    test creation

---
+++ tests/test_montecarlo.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import numpy as np
+
+from stopy.montecarlo import MonteCarlo
+
+
+class TestMonteCarlo(TestCase):
+    def test_mean(self):
+        mc = MonteCarlo()
+        self.assertEqual(mc.mean(lambda: 1.0), 1.0)
+        data = np.linspace(1, 11, 19)
+        self.assertEquals((mc.mean(lambda: data) - data).all(), 0.0)
+        self.assertAlmostEqual(mc.mean(lambda: np.random.normal(loc=3.5),
+                                       n_sim=10_000), 3.5, places=1)
+        self.assertAlmostEqual(mc.mean(np.random.normal, param={"loc": -0.2},
+                                       n_sim=100_000), -0.2, places=2)
+
+    def test_mean_with_variance(self):
+        mc = MonteCarlo()
+        self.assertEqual(mc.mean_with_variance(lambda: 1.0)[1], 0.0)
+        data = np.linspace(1, 11, 19)
+        self.assertEquals((mc.mean_with_variance(lambda: data)[1]).all(), 0.0)
+        self.assertAlmostEqual(mc.mean_with_variance(
+            lambda: np.random.normal(loc=3.5), n_sim=10_000)[1], 1.0, places=1)
+        self.assertAlmostEqual(mc.mean_with_variance(
+            np.random.normal, {"scale": 0.2}, n_sim=100_000)[1], 0.04, places=2)
+
--- tests/test_processes.py
+++ tests/test_processes.py
@@ -18,12 +18,6 @@ class TestWiener(TestCase):
         self.assertEqual(shape_time_20.shape, (10000, 5))
         self.assertEqual(shape_time_8coma5.shape, (4250, 6))
 
-    def test_generate_on_grid(self):
-        self.fail()
-
-    def test_generate_array(self):
-        self.fail()
-
 
 class TestPoisson(TestCase):
     def test__generate_incrementally(self):
@@ -41,62 +35,3 @@ class TestPoisson(TestCase):
         self.assertEqual(shape_div_200.shape, (200, 4))
         self.assertEqual(shape_time_20.shape, (10000, 5))
         self.assertEqual(shape_time_8coma5.shape, (4250, 6))
-
-    def test_generate_one_dim_jumps(self):
-        self.fail()
-
-    def test_generate_jumps(self):
-        self.fail()
-
-    def test_generate_on_grid(self):
-        self.fail()
-
-
-class TestOrnsteinUhlenbeck(TestCase):
-    def test_generate(self):
-        arguments = {"theta": 1.0, "mu": 0, "sigma": 1,
-                     "start_point": np.zeros(shape=(3,)),
-                     "time": 1, "x_axis": False, "division": 100}
-        shape_div_100 = OrnsteinUhlenbeck.generate_array(**arguments)
-        arguments["division"] = 200
-        arguments["start_point"] = np.zeros(shape=(4,))
-        shape_div_200 = OrnsteinUhlenbeck.generate_array(**arguments)
-        arguments["division"] = 500
-        arguments["time"] = 20
-        arguments["start_point"] = np.zeros(shape=(5,))
-        shape_time_20 = OrnsteinUhlenbeck.generate_array(**arguments)
-        arguments["time"] = 8.5
-        arguments["start_point"] = np.zeros(shape=(6,))
-        shape_time_8coma5 = OrnsteinUhlenbeck.generate_array(**arguments)
-        self.assertEqual(shape_div_100.shape, (100, 3))
-        self.assertEqual(shape_div_200.shape, (200, 4))
-        self.assertEqual(shape_time_20.shape, (10000, 5))
-        self.assertEqual(shape_time_8coma5.shape, (4250, 6))
-
-    def test_generate_on_grid(self):
-        self.fail()
-
-    def test_generate_array(self):
-        self.fail()
-
-
-class TestMemory(TestCase):
-    def test_Wiener(self):
-        self.fail()
-
-    def test_Poisson(self):
-        self.fail()
-
-    def test_OrnsteinUhlenbeck(self):
-        self.fail()
-
-
-class TimeComputing(TestCase):
-    def test_Wiener(self):
-        self.fail()
-
-    def test_Poisson(self):
-        self.fail()
-
-    def test_OrnsteinUhlenbeck(self):
-        self.fail()
